ff16124b51639c214f75d9c6ddb55f6a5f75f212
John Stevenson <john@jr0cket.co.uk>
2014-08-22 15:04:30 +0100

    Added Express framework and updated project information

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World Tour Paris");
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
